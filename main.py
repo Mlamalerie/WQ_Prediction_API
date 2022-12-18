9aa280f0be2460540b481d5007d2fd6ecd1e0931
Mⱥlamali The Engineer <56583866+Mlamalerie@users.noreply.github.com>
2022-12-18 23:44:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
